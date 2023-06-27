ba48cf86f8bd1e59278467f5b39b8143736da5d4
NBRPLONIX <119618511+NBRPLONIX@users.noreply.github.com>
2023-06-26 20:48:40 -0500

    original

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+function showAlert(){
+    alert("¡Hola, esta es una alerta desde javascript!");
+}
+
+const form = document.getElementById("miForm")
+
+function validateEmail(email){
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+}
+
+function validateForm(){
+    const input = document.getElementById("email")
+    const inputValue = input.value
+
+    if(!validateEmail(inputValue)){
+        alert('Por favor ingrese un correo electrónico válido. ')
+    } else {
+        alert('Correo electrónico enviado correctamente.')
+    }
+}
+
+form.addEventListener("submit", function(lala){
+    lala.preventDefault()
+    validateForm();
+})
+
+document.querySelector("button.button-menu-toggle").addEventListener("click",function(){
+    document.querySelector(".nav-links").classList.toggle("nav-links-responsive")
+})
+
+
+
 
